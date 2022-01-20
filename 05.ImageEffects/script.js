d2e8eeb79b4817ccb8dbc34f07ab906c9a52d6f4
LeCarteloo <filippapiernik1999@gmail.com>
2022-01-20 15:46:48 +0100

    Updated image effect

--- 05.ImageEffects/script.js
+++ 05.ImageEffects/script.js
@@ -1,29 +1,56 @@
 window.onload = () => {
 
-    const image = document.querySelector('.image');
+    const bridge = document.querySelector('.bridge');
 
-    image.addEventListener('mousemove', (event) => {
+    bridge.addEventListener('mousemove', (event) => {
         let x = event.offsetX;
         let y = event.offsetY;
 
-        const cardWidth = image.offsetWidth;
-        const cardHeight = image.offsetHeight;
+        const cardWidth = bridge.offsetWidth;
+        const cardHeight = bridge.offsetHeight;
 
-        const centerX = image.offsetLeft + cardWidth / 2;
-        const centerY = image.offsetTop + cardHeight / 2;
+        const centerX = bridge.offsetLeft + cardWidth / 2;
+        const centerY = bridge.offsetTop + cardHeight / 2;
 
         x = ((x - centerX) * 5 / (cardWidth / 2));
         y = -((y -  centerY) * 5 / (cardHeight / 2));
 
         console.log(x, y);
-        image.style.transition = ``;
-        image.style.transform = `perspective(1000px) rotateX(${x}deg) rotateY(${y}deg)`;
+        bridge.style.transition = ``;
+        bridge.style.transform = `perspective(1000px) rotateX(${x}deg) rotateY(${y}deg)`;
     });
 
-    image.addEventListener('mouseleave', (event) => {
-        image.style.transform = `rotateX(0deg) rotateY(0deg)`;
-        image.style.transition = `1s`;
+    bridge.addEventListener('mouseleave', (event) => {
+        bridge.style.transform = `rotateX(0deg) rotateY(0deg)`;
+        bridge.style.transition = `1s`;
 
     });
 
 };
+const list = document.querySelectorAll('.list li');
+const stripe = document.querySelector('.stripe-left');
+const stripeWidth = stripe.offsetHeight - list[list.length - 1].offsetHeight;
+
+let speedX = 1;
+initializeList();
+animateList();
+
+function initializeList() {
+    let position = 0;
+    for (const element of list) {
+        element.style.bottom = `${position -= 50}px`
+    }
+}
+
+function animateList() {
+    list.forEach(element => {
+        let positionX = parseInt(element.style.bottom);
+        positionX = positionX + speedX;
+        if (positionX > stripeWidth + 40) {
+            element.style.bottom = `-30px`
+            return;
+        }
+        element.style.bottom = `${positionX}px`
+    });
+    requestAnimationFrame(animateList)
+}
